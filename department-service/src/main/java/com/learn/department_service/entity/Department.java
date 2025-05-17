522df8e7a51b75c21dc5e0450bd76ce097910c77
Jayanth Kumar <j.ei7778@gmail.com>
2025-05-17 18:28:46 +0530

    Added department-service microservice

---
+++ department-service/src/main/java/com/learn/department_service/entity/Department.java
@@ -0,0 +1,28 @@
+package com.learn.department_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "department_name")
+    private String departmentName;
+
+    @Column(name = "department_description")
+    private String departmentDescription;
+
+    @Column(name = "department_code")
+    private String departmentCode;
+
+}
---
+++ department-service/src/main/java/com/learn/department_service/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.learn.department_service.repository;
+
+import com.learn.department_service.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
